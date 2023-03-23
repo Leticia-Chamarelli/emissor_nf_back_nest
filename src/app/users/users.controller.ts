2e1daed47745150744017de96d2fef1ab6c4b805
Leticia <leticia.chamarelli@outlook.com>
2023-03-22 21:52:06 -0300

    first commit

---
+++ src/app/users/users.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('api/v1/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async index() {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  async store(@Body() body: CreateUserDto) {
+    return this.usersService.store(body);
+  }
+
+  @Get(':id')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.usersService.findOneOrFail({id});
+  }
+
+  @Put(':id')
+  async update() {
+    return null;
+  }
+
+  @Delete(':id')
+  async destroy() {
+    return null;
+  }
+}
